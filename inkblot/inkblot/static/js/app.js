2f1e721d01e290005f7392aceb3b31f40ddd92dd
jeff <jeff@ubuntu.localdomain>
2013-04-12 07:29:16 -0700

    added emberjs UI stub

--- inkblot/inkblot/static/js/app.js
+++ inkblot/inkblot/static/js/app.js
@@ -1,4 +1,4 @@
-App = Ember.Application.create();
+window.App = App =  Ember.Application.create();
 
 App.Store = DS.Store.extend({
   revision: 12,
@@ -10,21 +10,22 @@ App.Store = DS.Store.extend({
 
 App.Router.map(function() {
   // put your routes here
-  this.resource('lessons', function (){
-    this.resource('lesson', { path: ':lesson_id'})
+  this.resource('lessons', { path: '/lessons'}, function() {
+    this.resource('lesson', { path: ':lesson_id'}, function() {
+      this.resource('tasks');     
+    });
   });
   this.resource('about');
 });
 
+// selects default tab when page loads
 App.IndexRoute = Ember.Route.extend({
   redirect: function () {
     this.transitionTo('lessons');
   }
 });
 
-App.Task = DS.Model.extend({
-  lesson: DS.belongsTo('App.Lesson'),
-  notes: DS.attr('string'),
+App.User = DS.Model.extend({
   
 });
 
@@ -32,13 +33,50 @@ App.Lesson = DS.Model.extend({
   tasks: DS.hasMany('App.Task'),
   title: DS.attr('string'),
   instruction: DS.attr('string'),
+  ltype: DS.attr('string')
 });
+
+App.Task = DS.Model.extend({
+  lesson: DS.belongsTo('App.Lesson'),
+  word: DS.attr('string'),
+  sound: DS.attr('string'),
+});
+
 
 App.LessonsRoute = Ember.Route.extend({
   model: function() {
     return App.Lesson.find();
     }
 });
+
+
+
+
+App.TaskView = Ember.View.extend ({
+  templateName: 'task-view',
+  attributeBindings: ['task-id','task-sound', 'task-word'],
+  'task-id': function() {
+        return this.content.id;
+  }.property('content.id'),
+  'task-sound': function() {
+        return this.content.sound;
+  }.property('content.sound'),
+  'task-word': function() {
+        return this.content.word;
+  }.property('content.word'),
+    
+  click: function(evt) {
+    debugger;
+    var soundfile = this.content.sound;
+    return;
+  }
+
+});
+
+App.Store.adapter.serializer.map('App.Lesson', {
+   tasks: {embedded: 'load'}
+});
+
 
 App.Lesson.FIXTURES = [{
   id: 1,
